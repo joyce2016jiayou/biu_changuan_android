b980b26b93ac305ee368c9837c0c37da371d6689
joyce <yangyanzhen@noplugins.onaliyun.com>
2019-07-12 10:58:52 +0800

    second_commit

---
+++ app/src/main/java/com/example/keepfit/KeepFitActivity.java
@@ -0,0 +1,15 @@
+package com.example.keepfit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KeepFitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_keepfit);
+
+
+    }
+}
