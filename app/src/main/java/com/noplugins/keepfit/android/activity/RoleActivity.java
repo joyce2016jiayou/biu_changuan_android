8ad5559d34b7687005f63c1ba952bcbd71062d20
sujinlin <jinlinsu@qq.com>
2019-08-14 17:17:23 +0800

    用户角色

--- app/src/main/java/com/noplugins/keepfit/android/activity/RoleActivity.java
+++ app/src/main/java/com/noplugins/keepfit/android/activity/RoleActivity.java
@@ -19,6 +19,7 @@ import com.noplugins.keepfit.android.R;
 import com.noplugins.keepfit.android.adapter.RoleAdapter;
 import com.noplugins.keepfit.android.base.BaseActivity;
 import com.noplugins.keepfit.android.entity.RoleBean;
+import com.noplugins.keepfit.android.util.data.SharedPreferencesHelper;
 import com.noplugins.keepfit.android.util.net.Network;
 import com.noplugins.keepfit.android.util.net.entity.Bean;
 import com.noplugins.keepfit.android.util.net.progress.ProgressSubscriberNew;
@@ -79,9 +80,9 @@ public class RoleActivity extends BaseActivity {
         ButterKnife.bind(this);
         isShowTitle(false);
         posts = new ArrayList<>();
-        tv_user_name.setText("1");
-        tv_user_phone.setText("2");
-        tv_zhiwei_name.setText("3");
+        tv_user_name.setText((String)SharedPreferencesHelper.get(getApplicationContext(),"username",""));
+        tv_user_phone.setText((String)SharedPreferencesHelper.get(getApplicationContext(),"phone",""));
+        tv_zhiwei_name.setText("场馆主");
         set_jiugongge_view();
 
         getBindingUserList();
