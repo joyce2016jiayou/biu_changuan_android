a43936973f4e92f5b016e04e51270cda4dc6ed61
sujinlin <jinlinsu@qq.com>
2019-11-26 17:55:14 +0800

    钱包明细

--- app/src/main/java/com/noplugins/keepfit/android/activity/mine/BillDetailInfoActivity.kt
+++ app/src/main/java/com/noplugins/keepfit/android/activity/mine/BillDetailInfoActivity.kt
@@ -37,6 +37,7 @@ class BillDetailInfoActivity : BaseActivity() {
 
     private fun setting(bean:BalanceListBean.ListBean){
         if(bean.type == 1){
+            title_tv.text ="提现详情"
             lv_1.visibility = View.VISIBLE
             lv_2.visibility = View.GONE
             tv_zcje.text ="¥${bean.finalMoney}"
@@ -44,31 +45,32 @@ class BillDetailInfoActivity : BaseActivity() {
             if (null != bean.cardName){
                 tv_txyh.text= bean.cardName
             }
-            tv_operate.text = "收入-银行"
+            tv_operate.text = "提现-${bean.cardName}"
             tv_dqzt.text= statusToString(bean.status)
             tv_zclb.text = typeToString(bean.type)
+            tv_money.text = "-${bean.finalMoney}"
         } else {
+            title_tv.text = "收入详情"
             lv_1.visibility = View.GONE
             lv_2.visibility = View.VISIBLE
-
             tv_srje.text = "¥${bean.finalMoney}"
             tv_kcmc.text = bean.courseName
             tv_cgmc.text = bean.areaName
-            tv_sksj.text = ""
+            tv_sksj.text = bean.createDate
             if (bean.type == 3){
                 tv_operate.text = "收入-私教服务"
-            } else {
+            } else if (bean.type == 4){
                 tv_operate.text = "收入-团课服务"
+            } else {
+                tv_operate.text = "收入-健身服务"
             }
-
             tv_srlb.text = typeToString(bean.type)
+            tv_money.text = "+${bean.finalMoney}"
         }
         Glide.with(this)
             .load(typeToDrawble(bean.type))
                 .placeholder(R.drawable.logo_gray)
             .into(iv_logo)
-
-        tv_money.text = "-${bean.finalMoney}"
     }
 
     private fun requestData() {
@@ -95,6 +97,7 @@ class BillDetailInfoActivity : BaseActivity() {
     private fun typeToDrawble(type: Int): Int {
         return when (type) {
             1 -> R.drawable.yinlian
+            2 -> R.drawable.cg_logo
             3 -> R.drawable.sijiao_logo
             4 -> R.drawable.team_logo
             else -> R.drawable.yinlian
@@ -105,6 +108,7 @@ class BillDetailInfoActivity : BaseActivity() {
 
         return when (type) {
             1 -> "银行卡提现"
+            2 -> "健身服务"
             3 -> "私教服务"
             4 -> "团课服务"
             else -> "未知错误"
