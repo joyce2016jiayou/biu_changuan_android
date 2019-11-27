d279629f091a941cd8baf23d510f640e74248912
sujinlin <jinlinsu@qq.com>
2019-11-27 17:30:55 +0800

    场馆端 我的场馆

---
+++ app/src/main/java/com/noplugins/keepfit/android/activity/mine/cg/VenueDetailActivity.kt
@@ -0,0 +1,24 @@
+package com.noplugins.keepfit.android.activity.mine.cg
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.noplugins.keepfit.android.R
+import com.noplugins.keepfit.android.base.BaseActivity
+
+class VenueDetailActivity : BaseActivity() {
+
+    override fun initBundle(parms: Bundle?) {
+    }
+
+    override fun initView() {
+        setContentView(R.layout.activity_venue_detail)
+    }
+
+    override fun doBusiness(mContext: Context?) {
+    }
+
+
+
+//    rec_left
+}
